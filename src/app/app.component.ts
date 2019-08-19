999ac1b3c1d278c004271d72d8f86921163c214a
MelisaCastro17 <52536973+MelisaCastro17@users.noreply.github.com>
2019-08-19 17:36:16 -0500

    proyecto de calculadora simple con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Calculadora';
+export class AppComponent 
+{
+  num1:number;
+  num2:number;
+  rta:number;
+  operacion(num1:number,num2:number,opera:string): number
+  {
+    switch(opera)
+    {
+      case "suma":
+        return this.rta = this.num1 + this.num2;
+      case "resta":
+        return this.rta = this.num1 - this.num2;
+      case "multiplica":
+        return this.rta = this.num1 * this.num2;
+      case "dividir":
+        if(this.num2 == 0)
+        {
+        alert("No se puede dividr por cero");
+        return null;
+        }
+        else
+        {
+        return this.rta = this.num1 / this.num2;
+        }
+      case "modulo":
+        return this.rta = this.num1 % this.num2;
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule
+{
+
+
+
+}
